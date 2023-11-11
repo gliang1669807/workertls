c183c405c007ebb9f04ca5ebdb43b797ebbf73f5
gliang1669807 <149281529+gliang1669807@users.noreply.github.com>
2023-11-11 23:24:54 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'e92712cb-0ce5-4445-a30b-06324ce0eefe';
 
-const proxyIPs = ['104.28.156.112'];
+const proxyIPs = ['8.222.224.17'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
